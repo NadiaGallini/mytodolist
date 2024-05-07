2f8d8feed00c768e0c8e9c4567a70cffa958b313
NadiaGallini <gallini.nadi@yandex.ru>
2024-05-07 11:52:38 +0300

    Отправка проекта

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="q-pa-lg">
-    <h5>About</h5>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus autem officia neque velit? Magnam soluta sint provident itaque fugiat nulla, accusamus illo dicta non quaerat, illum laboriosam laudantium! Deleniti, animi.</p>
+    <h5>Обо мне</h5>
+    <p>Радуюсь жизни.</p>
   </q-page>
 </template>
 
